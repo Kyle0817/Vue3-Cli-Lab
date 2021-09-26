c3cf04341d94e32799949c795248e69006c95eed
Kyle0817 <gn02025752@gmail.com>
2021-09-26 14:16:33 +0800

    練習cli環境變數，編譯設定檔

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <CardComp></CardComp>
   </div>
 </template>
+
+<script>
+import CardComp from "@/components/CardComp.vue";
+
+export default {
+  components: {
+    CardComp,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <Form @submit="onSubmit" v-slot="{ errors }">
+      {{ errors }}
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <Field
+          id="email"
+          name="email"
+          type="email"
+          class="form-control"
+          placeholder="請輸入 Email"
+          rules="email|required"
+          :class="{ 'is-invalid': errors['email'] }"
+          v-model="user.email"
+        ></Field>
+        <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
+      </div>
+
+      <div class="mb-3">
+        <label for="name" class="form-label">姓名</label>
+        <Field
+          id="name"
+          name="姓名"
+          type="text"
+          class="form-control"
+          placeholder="請輸入姓名"
+          v-model="user.name"
+          :class="{ 'is-invalid': errors['姓名'] }"
+          rules="required"
+        ></Field>
+        <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
+      </div>
+
+      <div class="mb-3">
+        <label for="phone" class="form-label">電話</label>
+        <Field
+          id="phone"
+          name="電話"
+          type="text"
+          class="form-control"
+          placeholder="請輸入電話"
+          :rules="isPhone"
+          v-model="phone"
+          :class="{ 'is-invalid': errors['電話'] }"
+        ></Field>
+        <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+      </div>
+
+      <div class="mb-3">
+        <label for="region" class="form-label">地區</label>
+        <select id="region" name="地區" class="form-control">
+          <option value="">請選擇地區</option>
+          <option value="台北市">台北市</option>
+          <option value="高雄市">高雄市</option>
+        </select>
+        <span class="invalid-feedback"></span>
+      </div>
+
+      <div class="mb-3">
+        <label for="address" class="form-label">地址</label>
+        <input
+          id="address"
+          name="地址"
+          type="text"
+          class="form-control"
+          v-model="user.address"
+          placeholder="請輸入地址"
+        />
+        <span class="invalid-feedback"></span>
+      </div>
+
+      <button class="btn btn-primary" type="submit">Submit</button>
+    </Form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      user: {
+        email: "",
+        name: "",
+        address: "",
+        phone: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.user);
+    },
+    isPhone(value) {
+      const phoneNumber = /^(09)[0-9]{8}$/;
+      return phoneNumber.test(value) ? true : "需要正確的電話號碼";
+    },
+  },
+  created() {
+    console.log(this);
+  },
+};
 </script>
